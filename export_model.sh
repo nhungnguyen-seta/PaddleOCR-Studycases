dae9c530b15b3691c891c1f9d3ce330da0e95bde
nhungnguyen-seta <nhung.nguyen@setacinq.vn>
2022-10-17 11:03:50 +0700

    update checkpoint 14102022

---
+++ export_model.sh
@@ -0,0 +1,1 @@
+python3 tools/export_model.py -c configs/rec/en_PP-OCRv3_rec_train.yml -o Global.pretrained_model=output/PPOCRv3_rec_carmodels_14102022/best_accuracy  Global.save_inference_dir=./inference/PPOCRv3_rec_carmodels_14102022_infer/
--- train.sh
+++ train.sh
@@ -1,2 +1,1 @@
-# recommended paddle.__version__ == 2.0.0
-python3 -m paddle.distributed.launch --log_dir=./debug/ --gpus '0,1,2,3,4,5,6,7'  tools/train.py -c configs/rec/rec_mv3_none_bilstm_ctc.yml
+python3 tools/train.py -c configs/rec/en_PP-OCRv3_rec_train.yml -o Global.pretrained_model=./pretrain_models/en_PP-OCRv3_rec_train/best_accuracy
